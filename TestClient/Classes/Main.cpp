a818020cdfa97e2b3228ed9352611c11f5ec9543
alenming <635459675@qq.com>
2021-04-25 16:17:15 +0800

    ad
    ad

--- TestClient/Classes/Main.cpp
+++ TestClient/Classes/Main.cpp
@@ -1,58 +1,32 @@
+#include <vector>
 #include <iostream>
-#include <string>
-#include <ctime> 
 #include "TestClient.h"
 #include "GameNetworkNode.h"
-
-#include "Lua/LuaRegiest.h"
-#include "Lua/LuaStack_.h"
-
-#include "log/LogFileHandler.h"
-#include "log/LogConsoleHandler.h"
-#include "log/LogManager.h"
-
-
 using namespace std;
 
 
-int main(int argc, char ** argv) 
+int main()
 {
-	
-	//TestClient::getInstance()->onServerInit();
-	//auto poll = CGameNetworkNode::getInstance()->getPoller();
-	//poll->poll();
-
-	//Delay(3 * 1000);   //延时5秒
-	//TestClient::getInstance()->login();
-
-	//while (true)
-	//{
-	//	poll->poll();
-	//}
-
-	//char temp = ' ';
-	//cout << "登录中,任意键关闭" << endl;
-	//scanf("%c", &temp);
- //   TestClient::destroy();
-
-	LogManager::getInstance()->setShowTime(false);
-	LogManager::getInstance()->setShowDate(false);
-	LogManager::getInstance()->addHandler(1, new LogConsoleHandler());
-	LogFileHandler* pFileHandle = new LogFileHandler();
-	pFileHandle->setFilePath("../bin/");
-	pFileHandle->setNewFile(true);
-	pFileHandle->setFileName("TestClientOut");
-	pFileHandle->setFastModel(false);
-	LogManager::getInstance()->addHandler(2, pFileHandle);
-
-	regiestLuaFunction();
-	LuaStack_::getInstance()->executeScriptFile("../LuaSupport/Test/Test.lua");
-	LogManager::destroy();
-
-
+	TestClient::getInstance()->onServerInit();
+	TestClient::getInstance()->login();
+	auto clientPoll = CGameNetworkNode::getInstance()->getPoller();
 	while (true)
 	{
-
+		clientPoll->poll();
 	}
-    return 0;
+	//LogManager::getInstance()->setShowTime(false);
+	//LogManager::getInstance()->setShowDate(false);
+	//LogManager::getInstance()->addHandler(1, new LogConsoleHandler());
+	//LogFileHandler* pFileHandle = new LogFileHandler();
+	//pFileHandle->setFilePath("../bin/");
+	//pFileHandle->setNewFile(true);
+	//pFileHandle->setFileName("TestClientOut");
+	//pFileHandle->setFastModel(false);
+	//LogManager::getInstance()->addHandler(2, pFileHandle);
+
+	//regiestLuaFunction();
+	//LuaStack_::getInstance()->executeScriptFile("../LuaSupport/Test/Test.lua");
+	LogManager::destroy();
+	TestClient::destroy();
+	return 0;
 }
